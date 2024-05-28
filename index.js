343ce95cadf5ddb818b99046c3abc862632eee64
Anil <kanilreddy867@gmail.com>
2024-05-28 17:29:00 +0530

    updated the default route.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.use("/", (req,res) => {
+app.get("/", (req,res) => {
   res.send(`
     <h1>Welcome to FinTrack</h1>
     <p>Your personal finance companion</p>
